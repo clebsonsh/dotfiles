52512c24df20da3c0dfbded08b706a0b94a7c6bc
Clebson Moura <clebson.sh@gmail.com>
2024-07-06 18:51:30 -0300

    hide command pallet

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -11,6 +11,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.cursorline = true
+vim.opt.cmdheight = 0
 
 vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
 
