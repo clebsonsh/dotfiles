107488079fbd18f9ec63cba40835ef937967a7d6
Clebson Moura <clebson.sh@gmail.com>
2024-07-16 18:24:31 -0300

    add simple plugins that don't need config

---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,13 @@
+return {
+    -- Commenting support.
+    { "tpope/vim-commentary" },
+
+    -- Add, change, and delete surrounding text.
+    { "tpope/vim-surround" },
+
+    -- Indent autodetection with editorconfig support.
+    { "tpope/vim-sleuth" },
+
+    -- Allow plugins to enable repeating of commands.
+    { "tpope/vim-repeat" },
+}
