02ca4131677b8fc287c9e118bf297deef20c1315
Clebson Moura <clebson.sh@gmail.com>
2024-07-16 01:11:32 -0300

    make floaterm float

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -1,8 +1,9 @@
 return {
     "voldikss/vim-floaterm",
     config = function()
-        vim.g.floaterm_height = 0.7
-        vim.g.floaterm_wintype = "split"
+        vim.g.floaterm_title = ''
+        vim.g.floaterm_height = 0.85
+        vim.g.floaterm_width = 0.85
         vim.keymap.set("n", "<F1>", ":FloatermToggle<CR>")
         vim.keymap.set("t", "<F1>", "<C-\\><C-n>:FloatermToggle<CR>")
     end
