12fffc6d5f591c9eb40c7d85e44fbfa109b9369f
Clebson Moura <clebson.sh@gmail.com>
2024-07-16 18:23:45 -0300

    lualine rename and cleanup

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        sections = {
+            lualine_a = {'branch'},
+            lualine_b = {'diff', 'diagnostics'},
+            lualine_c = {},
+            lualine_x = {'filename'},
+            lualine_y = {'location'},
+            lualine_z = {'mode'},
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+    },
+}
