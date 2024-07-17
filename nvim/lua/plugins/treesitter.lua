80c58f0ed3949520ac1156bb73dab4a158fe46eb
Clebson Moura <clebson.sh@gmail.com>
2024-07-17 18:38:55 -0300

    let treesitter auto install any syntax highlight it come across

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,19 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	init = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		init = function()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+			configs.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = true,
+	},
 }
