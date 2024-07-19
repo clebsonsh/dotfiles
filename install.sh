7fc3e309e32fdadacf16055ece1ab84d9e1bad0c
Clebson Moura <clebson.sh@gmail.com>
2024-07-18 21:23:22 -0300

    add lazygit and oh-my-posh catppuccin theme

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ ln -s $DOTFILES/kitty $HOME/.config/kitty
 
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
+
+rm -rf $HOME/.config/lazygit
+ln -s $DOTFILES/nvim $HOME/.config/lazygit
+
+rm -rf $HOME/.config/omp
+ln -s $DOTFILES/nvim $HOME/.config/omp
