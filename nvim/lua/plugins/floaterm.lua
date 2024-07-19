8d0d87a485ebe5ec679ebf5ae7c49f34cb751bf8
Clebson Moura <clebson.sh@gmail.com>
2024-07-18 21:20:52 -0300

    add rounded border to floterm

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -4,6 +4,8 @@ return {
 		vim.g.floaterm_title = ""
 		vim.g.floaterm_height = 0.85
 		vim.g.floaterm_width = 0.85
+		vim.g.floaterm_borderchars = "─│─│╭╮╯╰"
+
 		vim.cmd(":hi FloatermBorder guibg=#1e1e2f guifg=#89b4fb")
 		vim.keymap.set("n", "<F1>", ":FloatermToggle<CR>")
 		vim.keymap.set("t", "<F1>", "<C-\\><C-n>:FloatermToggle<CR>")
