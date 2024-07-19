78f5ef66ec7eb2924229218ecce1e6e67eb69f12
Clebson Moura <clebson.sh@gmail.com>
2024-07-18 21:16:40 -0300

    swith to catppuccin theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,43 @@
 return {
-	"Mofiqul/vscode.nvim",
-	name = "vscode",
-	opts = {
-		-- Underline `@markup.link.*` variants
-		underline_links = true,
+	{
+		"Mofiqul/vscode.nvim",
+		enabled = false,
+		name = "vscode",
+		opts = {
+			-- Underline `@markup.link.*` variants
+			underline_links = true,
 
-		-- Disable nvim-tree background color
-		disable_nvimtree_bg = true,
+			-- Disable nvim-tree background color
+			disable_nvimtree_bg = true,
+		},
+		init = function()
+			vim.cmd.colorscheme("vscode")
+		end,
 	},
-	init = function()
-		vim.cmd.colorscheme("vscode")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			term_colors = true,
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+			},
+			highlight_overrides = {
+				mocha = function(mocha)
+					return {
+						NvimTreeNormal = { bg = mocha.none },
+					}
+				end,
+			},
+		},
+		init = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
