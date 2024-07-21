23e54e663f7185481393a5aae89023a79ce2730d
Clebson Moura <clebson.sh@gmail.com>
2024-07-21 18:48:06 -0300

    change keymaps that crashed with harpoon

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,11 +1,11 @@
 return {
 	"vim-test/vim-test",
 	init = function()
-		vim.keymap.set("n", "<Leader>t", ":TestNearest<CR>")
+		vim.keymap.set("n", "<Leader>t", ":TestNearest<CR><CR>")
 		vim.keymap.set("n", "<Leader>T", ":TestFile<CR>")
-		vim.keymap.set("n", "<Leader>a", ":TestSuite<CR>")
-		vim.keymap.set("n", "<Leader>l", ":TestLast<CR>")
-		vim.keymap.set("n", "<Leader>g", ":TestVisit<CR>")
+		vim.keymap.set("n", "<Leader>s", ":TestSuite<CR>")
+		vim.keymap.set("n", "<Leader>p", ":TestLast<CR>")
+		vim.keymap.set("n", "<Leader>o", ":TestVisit<CR>")
 		vim.cmd("let test#php#phpunit#executable = './vendor/bin/pest'")
 		vim.cmd("let test#strategy = 'floaterm'")
 
