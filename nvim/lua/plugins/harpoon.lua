f014bae2c6e4c84e8bc1864053e48692ef76944a
Clebson Moura <clebson.moura@exata.it>
2024-08-09 15:01:00 -0300

    using same keybinds from vscode config

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -16,23 +16,23 @@ return {
 			ui_width_ratio = 0.40,
 		}
 
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<Leader>h", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
 		end)
 
-		vim.keymap.set("n", "<Leader>h", function()
+		vim.keymap.set("n", "<Leader>1", function()
 			harpoon:list():select(1)
 		end)
 
-		vim.keymap.set("n", "<Leader>j", function()
+		vim.keymap.set("n", "<Leader>2", function()
 			harpoon:list():select(2)
 		end)
 
-		vim.keymap.set("n", "<Leader>k", function()
+		vim.keymap.set("n", "<Leader>3", function()
 			harpoon:list():select(3)
 		end)
 
-		vim.keymap.set("n", "<Leader>l", function()
+		vim.keymap.set("n", "<Leader>4", function()
 			harpoon:list():select(4)
 		end)
 	end,
