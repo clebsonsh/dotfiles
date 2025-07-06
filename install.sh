d66464c787bee0fc17b02b7afa3341019ceec40e
Clebson Moura <clebson.sh@gmail.com>
2025-07-06 18:53:20 -0300

    fix: update lazygit config path

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
 
 rm -rf $HOME/.config/lazygit
-ln -s $DOTFILES/nvim $HOME/.config/lazygit
+ln -s $DOTFILES/lazygit $HOME/.config/lazygit
 
 rm -rf $HOME/.config/omp
 ln -s $DOTFILES/omp $HOME/.config/omp
