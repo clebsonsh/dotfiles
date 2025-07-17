f0fa0a8c8147c586153e7ddcb5aaa66366de7229
Clebson Moura <clebson.sh@gmail.com>
2025-07-16 22:01:29 -0300

    update nvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,16 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-
-vim.filetype.add({
-  extension = {
-    gotmpl = "gotmpl",
-  },
-  pattern = {
-    [".*/templates/.*%.tpl"] = "helm",
-    [".*/templates/.*%.ya?ml"] = "helm",
-    ["helmfile.*%.ya?ml"] = "helm",
-  },
-})
-
-vim.g.lazyvim_php_lsp = "intelephense"
