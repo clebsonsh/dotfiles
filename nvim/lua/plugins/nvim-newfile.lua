650ef290aa84e56d05acacd0d925696c12cd23f4
Clebson Moura <clebson.sh@gmail.com>
2025-08-03 23:25:18 -0300

    add keybinds to create new file

--- nvim/lua/plugins/nvim-newfile.lua
+++ nvim/lua/plugins/nvim-newfile.lua
@@ -1,11 +1,14 @@
 return {
-    "adibhanna/nvim-newfile.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-        require("nvim-newfile").setup({
-            notifications = {
-                enabled = false, -- Set to false to disable file creation notifications
-            },
-        })
-    end,
+  "adibhanna/nvim-newfile.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  config = function()
+    require("nvim-newfile").setup({
+      notifications = {
+        enabled = false, -- Set to false to disable file creation notifications
+      },
+    })
+
+    vim.keymap.set("n", "<leader>nf", ":NewFile<CR>", { desc = "Create new file" })
+    vim.keymap.set("n", "<leader>nh", ":NewFileHere<CR>", { desc = "Create new file here" })
+  end,
 }
