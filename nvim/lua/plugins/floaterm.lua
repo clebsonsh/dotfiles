83495166f1c228d7402230777ff05fa44b43563a
Clebson Moura <clebson.sh@gmail.com>
2025-08-03 23:25:02 -0300

    change floaterm config

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -1,12 +1,10 @@
 return {
   "voldikss/vim-floaterm",
   init = function()
+    vim.g.floaterm_wintype = "split"
     vim.g.floaterm_title = ""
-    vim.g.floaterm_height = 0.95
-    vim.g.floaterm_width = 0.95
+    vim.g.floaterm_height = 0.99
 
-    vim.cmd(":hi FloatermBorder guibg=#1e1e2f guifg=#89b4fb")
-
-    vim.g.floaterm_keymap_toggle = '<C-j>'
+    vim.g.floaterm_keymap_toggle = "<C-j>"
   end,
 }
