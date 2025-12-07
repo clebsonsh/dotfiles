5a7b70dbd41e59991dfd1bbc13c9c8f30779ef8e
Clebson Moura <clebson.sh@gmail.com>
2025-12-07 14:19:47 -0300

    simplify config

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # Clebson's Dotfiles
 
 > "These configs are dialed in on my machine; no guarantees they'll groove on yours!"
-
-These dotfiles are my go-to for turbocharging my coding flow, cobbled together from a mix of tutorials that got my brain buzzing.
-
-## What Sparked This Madness
-
-- [ThePrimeagen (Youtube) - Vim As Your Editor](https://www.youtube.com/watch?v=X6AR2RMB5tE)
-- [Jess Archer (Laracasts) - Neovim as a PHP and JavaScript IDE](https://laracasts.com/series/neovim-as-a-php-ide)
-- [Typecraft (Youtube) - Neovim from scratch](https://www.youtube.com/watch?v=zHTeCSVAFNY)
-- [Adib Hanna - The Laravel Neovim Setup ](https://www.youtube.com/watch?v=OCKuoEOQ_BA)
-
-These legends lit the fire and showed me the ropes to craft a setup that's all about coding like a boss.
--- install.sh
+++ install.sh
@@ -3,28 +3,33 @@
 DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 rm -rf "$HOME"/.config/kitty
-ln -s "$DOTFILES"/kitty "$HOME"/.config/kitty
-
-rm -rf "$HOME"/.config/nvim
-ln -s "$DOTFILES"/nvim "$HOME"/.config/nvim
-
-rm -rf "$HOME"/.config/zed
-ln -s "$DOTFILES"/zed "$HOME"/.config/zed
+mkdir "$HOME"/.config/kitty
+ln -rs "$DOTFILES"/config/kitty.conf "$HOME"/.config/kitty/kitty.conf
 
 rm -rf "$HOME"/.config/lazygit
-ln -s "$DOTFILES"/lazygit "$HOME"/.config/lazygit
+mkdir "$HOME"/.config/lazygit
+ln -rs "$DOTFILES"/config/lazygit.yml "$HOME"/.config/lazygit/config.yml
 
 rm -rf "$HOME"/.config/starship.toml
-ln -s "$DOTFILES"/misc/starship.toml "$HOME"/.config/starship.toml
+ln -s "$DOTFILES"/config/starship.toml "$HOME"/.config/starship.toml
 
 rm -rf "$HOME"/.gitconfig
-ln -s "$DOTFILES"/misc/gitconfig "$HOME"/.gitconfig
-
-rm -rf "$HOME"/.ideavimrc
-ln -s "$DOTFILES"/misc/ideavimrc "$HOME"/.ideavimrc
+ln -s "$DOTFILES"/config/gitconfig "$HOME"/.gitconfig
 
 rm -rf "$HOME"/.vimrc
-ln -s "$DOTFILES"/misc/vimrc "$HOME"/.vimrc
+ln -s "$DOTFILES"/config/vimrc "$HOME"/.vimrc
 
 rm -rf "$HOME"/.bashrc
-ln -s "$DOTFILES"/misc/bashrc "$HOME"/.bashrc
+ln -s "$DOTFILES"/config/bashrc "$HOME"/.bashrc
+
+rm -rf "$HOME"/.config/Code/User/settings.json
+ln -s "$DOTFILES"/config/vscode.json "$HOME"/.config/Code/User/settings.json
+
+code --install-extension adpyke.codesnap
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension catppuccin.catppuccin-vsc-icons
+code --install-extension ms-azuretools.vscode-containers
+code --install-extension tobias-z.vscode-harpoon
+code --install-extension vscodevim.vim
+code --install-extension xdebug.php-debug
